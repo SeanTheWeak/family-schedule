5d4776e30ec7bb49342f58b7401ae021997e3016
SeanTheWeak <kwistianuslee@gmail.com>
2026-07-18 15:41:06 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
-const SUPABASE_URL = "https://sxfyzbjkmyhculyazaak.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4Znl6YmprbXloY3VseWF6YWFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2NDY1MDcsImV4cCI6MjA5ODIyMjUwN30.FaPUiY3tO1SSu74mfkr5tGUM9cOeUfy-g4qR5flpJGs";
+const SUPABASE_URL = "https://kubuypsabeimcgqzwsgu.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1YnV5cHNhYmVpbWNncXp3c2d1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQzNTY3NDMsImV4cCI6MjA5OTkzMjc0M30.q2kcloyNhV5Qw3cToBidaLYhTp126QwyRIIrThjXets";
 const ADMIN_PIN = "8008";
 
 const HEADERS = {
